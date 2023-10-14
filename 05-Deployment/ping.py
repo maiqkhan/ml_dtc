f997aa512de7504404904fc58e2126ac10d6a078
Mustafa <mkhan2431@email.com>
2023-10-14 13:57:12 -0400

    add ping route

--- 05-Deployment/ping.py
+++ 05-Deployment/ping.py
@@ -3,10 +3,12 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get('/ping')
+@app.get("/ping")
 def ping():
-    return 'PONG'
+    return "PONG"
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
